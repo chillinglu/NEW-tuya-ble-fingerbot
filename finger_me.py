63eb382026a0e26a9dfcef42e21de428819c41b9
chillinglu <45127677+chillinglu@users.noreply.github.com>
2025-11-13 15:58:39 +0100

    Update device credentials in finger_me.py

--- finger_me.py
+++ finger_me.py
@@ -7,10 +7,11 @@ from Crypto.Cipher import AES
 from binascii import unhexlify, hexlify
 
 # Use https://github.com/redphx/tuya-local-key-extractor to get these values
-LOCAL_KEY = ''
-MAC = ''
-UUID = ''
-DEV_ID = ''
+#LOCAL_KEY = '`$u|?u.p={7me[:<'
+LOCAL_KEY = '5T_SokY+GPk_uhME'
+MAC = 'DC:23:52:10:20:74'
+UUID = 'uuid819926cef77c'
+DEV_ID = 'bf5c9789wbrmcb5w'
 
 fingerbot = FingerBot(MAC, LOCAL_KEY, UUID, DEV_ID)
 fingerbot.connect()
